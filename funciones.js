16abbcf3ec686b0effdf26e9c9b29abf71e6c33a
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-02 15:03:41 -0600

    se agregaron las imágenes al juego

---
+++ funciones.js
@@ -0,0 +1,15 @@
+/*Inicio*/
+window.onload = function () {
+	canvas = document.getElementById("miCanvas");
+	if (canvas && canvas.getContext) {
+		ctx = canvas.getContext("2d");
+		if (ctx) {
+			x = canvas.width / 2;
+			if (mensaje("INVADERS"));	
+			imgNave = new Image();
+			imgOvni = new Image();
+			imgOvni.src = "imagenes/ovni.png";
+			imgNave.src = "imagenes/nave.png";
+			imgNave.onload = function () {
+				nave = new nave(0);
+			}
