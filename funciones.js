77567b77bb8d66060b3e3b5cedb8134a0b536b80
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-02 15:47:29 -0600

    se agrego disparo

--- funciones.js
+++ funciones.js
@@ -12,4 +12,18 @@ window.onload = function () {
 			imgNave.src = "imagenes/nave.png";
 			imgNave.onload = function () {
 				nave = new nave(0);
+            }
+            imgOvni.onload = function () {
+				for (var i = 0; i < 5; i++) {
+					for (var j = 0; j < 10; j++) {
+						ovnis_array.push(new Enemigo(100 + 40 * j, 30 + 45 * i));
+					}
+				}
+				setTimeout(anima, 1500);
+				disparoEnemigo = setTimeout(disparaEnemigo, tiempoDisparo);
 			}
+		} else {
+			alert("Error");
+		}
+	}
+}
