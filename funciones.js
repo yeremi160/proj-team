e0e348d4907087f3df920192868f91d29eed705c
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-02 21:22:28 -0600

    animaciones de la nave, balas

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-/*Inicio*/
+
 //se agregaron canvas
 window.onload = function () {
 	canvas = document.getElementById("miCanvas");
@@ -30,7 +30,7 @@ window.onload = function () {
 		}
 	}
 }
-/*Listener, Animaciones, correcta funcion de los distintos navegadores*/
+//funciones para navegadores
 window.requestAnimationFrame = (function () {
 	return window.requestAnimationFrame ||
 		window.webkitRequestAnimationFrame ||
@@ -45,7 +45,6 @@ document.addEventListener("keyup", function (e) {
 	tecla[e.keyCode] = false;
 });
 //variables para mover nave,etc
-/*Variables*/
 var canvas, ctx;
 var x = 100;
 var y = 100;
@@ -68,3 +67,48 @@ var endGame = false;
 var disparoEnemigo;
 var tiempoDisparo = 400;
 var puntos = 0;
+//Objetos balas y nave
+function Bala(x, y, w) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.dibuja = function () {
+		ctx.save();
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.w, this.w);
+		this.y = this.y - 6;
+		ctx.restore();
+	};
+	this.dispara = function () {
+		ctx.save();
+		ctx.fillStyle = "yellow";
+		ctx.fillRect(this.x, this.y, this.w, this.w);
+		this.y = this.y + 4;
+		ctx.restore();
+	};
+}
+//Nave y animaciones
+function nave(x) {
+	this.x = x;
+	this.y = 450;
+	this.w = 30;
+	this.h = 15;
+	this.dibuja = function (x) {
+		this.x = x;
+		if(imgAni2 < 5){
+			ctx.drawImage(imgNave,0   , 0   , 32  , 32  , this.x, this.y, 35  , 35);
+			imgAni2 = imgAni2 + 1;
+			imgAni = imgAni + 1;
+			checarBalas();
+//balas
+		} else if(imgAni2 < 10) {
+			ctx.drawImage(imgNave,32  , 0   , 32  , 32  , this.x, this.y, 35  , 35);
+			imgAni2 = imgAni2 + 1;
+			imgAni = imgAni + 1;
+		} else{
+			ctx.drawImage(imgNave,32  , 0   , 32  , 32  , this.x, this.y, 35  , 35);
+			imgAni2 = 0;
+		}
+		
+	};
+}
