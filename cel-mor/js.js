3e7486666e3db42ef0d0c2ff8ea9941ffb6bf830
Célita Morales <cmoralesm37@miumg.edu.gt>
2020-06-08 04:49:50 -0600

    se completa función de frecuencia según nota musical

--- cel-mor/js.js
+++ cel-mor/js.js
@@ -1,4 +1,12 @@
 var context= new AudioContext();
 function notamusical(frecuencia){
     var o=context.createOscillator();
+    g= context.createGain();
+    o.connect(g);
+    o.type="sawtooth";
+    o.frequency.value=frecuencia;
+    g.connect(context.destination);
+    o.start(0);
+     g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+
 }
