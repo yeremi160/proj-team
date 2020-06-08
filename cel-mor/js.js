c491f90bc06bab5706f920cd38b83212e94154eb
Célita Morales <cmoralesm37@miumg.edu.gt>
2020-06-08 06:22:12 -0600

    Proceso terminado, faltan detalles

--- cel-mor/js.js
+++ cel-mor/js.js
@@ -1,13 +1,13 @@
 var context= new AudioContext();
-
+//FUNCIÓN DE FRECUENCIA CON SONIDOS INCLUÍDOS EN JAVA
 function notamusical(frecuencia){
-    var o=context.createOscillator();
-    g= context.createGain();
-    o.connect(g);
-    o.type="sawtooth";
-    o.frequency.value=frecuencia;
-    g.connect(context.destination);
-    o.start(0);
-    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+    var x=context.createOscillator();
+    y= context.createGain();
+    x.connect(y);
+    x.type="sawtooth";
+    x.frequency.value=frecuencia;
+    y.connect(context.destination);
+    x.start(0);
+    y.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
 
 }
