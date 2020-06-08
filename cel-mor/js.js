203efe48c501abc83208dc432bc2d9f1e921ef8f
Célita Morales <cmoralesm37@miumg.edu.gt>
2020-06-08 03:53:06 -0600

    Se corrigieron algunos errores

--- cel-mor/js.js
+++ cel-mor/js.js
@@ -1,79 +1,55 @@
 //FUNCIÓN PARA QUE INTERPRETE UNA SEÑAL DEL TECLADO y salte-------------------------
-document.addEventListener('keydown', funcion(evento)
-{
-    if(evento.keycode == 32){
+document.addEventListener( 'keydown', function(evento){
+    if(evento.keyCode == 32){
         console.log("salta");
     }
 });
 
-
-//FIN DE FUNCION SALTAR -------------------------------------------------
 //variables para las imágenes del juego
-var virus, antiv, suelo, nube;
-//funcion para llamar a las imagenes
+var virus, antiv, suelo;
 function cargaimagenes(){
     virus = new image();
     antiv = new image();
     suelo = new image();
-    nube = new image();
     
     virus.src = 'img/covid1.png';
     antiv.src = 'img/antiv1.png';
     suelo.src = 'img/suelo.png';
-    nube.src = 'img/nube.png';
 }
-
 
 //asignación de variables para ciertas funciones
 var ancho=700;
 var alto=300;
 //variables para canvas y para contexto
 var canvas, ctx;
-
+//varible canvas obteniendo id canvas de html
 function inicializa(){
-    //varible canvas obteniendo id canvas de html
     canvas = document.getElementById('canvas');
     ctx = canvas.getcontext('2d');
-    cargaimagenes()
+    cargaimagenes();
+    
 }
 
 //función para borrar CANVAS, la más sencilla, pasando de una variable a otra
 function borracanvas(){
-    canvas.width = ancho;
-    canvas.height =alto;
+    canvas.width= ancho;
+    canvas.height= alto;
 }
 
-//función para dibujar antivirus
 function dibujaantiv(){
-
+    ctx.drawimage(antiv,0,0,60,120,100,100,59,110);
 }
-
-
 
 //BUCLE para que el juego se repita--------------------------------------------------
+//en esta parte, se irá llamando a todas las funciones mientras el juego se repita
 var FPS = 10;
 //INTERVALO DE TIEMPO y función principal
 setInterval(function(){
     principal();
 },1000/10);
 
-
 function principal(){
     borracanvas();
     dibujaantiv();
+    
 }
-
-
-
-
-
-
-//PRIMERAS EDICIONES borrar de último o dejar, dependiendo... O.O   O.O   O.O   O.O 
-const canvas = document.getElementById("juego");
-const context = canvas.getcontext("2d");
-context.scale(20, 20);
-
-//color y tamaño de canvas 0.0   0.0   0.0   0.0   0.0   0.0   0.0   0.0 
-
-context.fillStyle = "#000";
-context.fillRect(0,0, canvas.width, canvas.height);
